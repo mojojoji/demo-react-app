598a9e9706c09a0f908076120b4c9f97e9af5414
Joji Augustine <jojimail@gmail.com>
2022-01-29 01:21:50 +0530

    Update react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +11,11 @@ export default function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>Counter : {counter}</h2>
+        <div className="button-container">
+          <button onClick={() => setCounter(c => c - 1)}>Decrement</button>
+          <button onClick={() => setCounter(c => c + 1)}>Increment</button>
+        </div>
       </header>
     </div>
   );
